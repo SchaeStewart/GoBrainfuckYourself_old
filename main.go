848507ad9a5f9c287c7f582c8dd5109b03ff03c7
Schaffer Stewart <Schaffer.Stewart@ibm.com>
2019-08-20 21:09:08 -0400

    Add Memory datastruct. Add Left and Right methods

--- main.go
+++ main.go
@@ -18,6 +18,32 @@ func (c *Cell) Decrement() {
 	c.Value--
 }
 
+type Memory struct {
+	Cells       []Cell
+	CurrentCell int
+}
+
+// TODO: should be able to pass in default length
+func NewMemory(size ...int) *Memory {
+	return &Memory{
+		Cells: make([]Cell, 300000),
+	}
+}
+
+func (m *Memory) GetCurrentCell() *Cell {
+	return &m.Cells[m.CurrentCell]
+}
+
+func (m *Memory) Right() int {
+	m.CurrentCell++
+	return m.Cells[m.CurrentCell].Value
+}
+
+func (m *Memory) Left() int {
+	m.CurrentCell--
+	return m.Cells[m.CurrentCell].Value
+}
+
 func main() {
 	fmt.Println("hello")
 }
--- main__test.go
+++ main__test.go
@@ -19,3 +19,24 @@ func TestCellDecrement(t *testing.T) {
 		t.Errorf("Expected cell to equal -1 instead got %d", cell.Value)
 	}
 }
+
+func TestCellsRight(t *testing.T) {
+	m := NewMemory()
+	previousCell := m.GetCurrentCell()
+	m.Right()
+	if m.GetCurrentCell() == previousCell {
+		t.Errorf("Current cell did not change")
+	}
+}
+
+func TestCellsLeft(t *testing.T) {
+	m := NewMemory()
+	// Setup
+	m.Right()
+
+	previousCell := m.GetCurrentCell()
+	m.Left()
+	if m.GetCurrentCell() == previousCell {
+		t.Errorf("Current cell did not change")
+	}
+}
