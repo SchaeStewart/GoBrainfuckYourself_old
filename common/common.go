82c2e232b5050b55fd0129e644ade2f12b26c0a1
Schaffer Stewart <Schaffer.Stewart@ibm.com>
2019-08-26 21:18:41 -0400

    Convert to iota

--- common/common.go
+++ common/common.go
@@ -5,19 +5,19 @@ type Commands int
 
 const (
 	// Left moves the operator left
-	Left Commands = 0
+	Left Commands = iota + 1
 	// Right moves the operator right
-	Right Commands = 1
+	Right
 	// Inc increases the current cell by 1
-	Inc Commands = 2
+	Inc
 	// Dec decreases the current cell by 1
-	Dec Commands = 3
+	Dec
 	// Get gets input from IO
-	Get Commands = 4
+	Get
 	// Put puts current cell to IO
-	Put Commands = 5
+	Put
 	// LoopStart denotes the start of a loop
-	LoopStart Commands = 6
+	LoopStart
 	// LoopEnd denotes the end of a loop
-	LoopEnd Commands = 7
+	LoopEnd
 )
