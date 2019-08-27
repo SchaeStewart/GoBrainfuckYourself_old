afa6b5983dc44b9d841a17352555d5e5e3453469
Schaffer Stewart <Schaffer.Stewart@ibm.com>
2019-08-26 21:19:20 -0400

    Add tests and functionallity for AddCommand. Auto calculate depth

--- queue/queue.go
+++ queue/queue.go
@@ -9,7 +9,9 @@ type queueItems struct {
 
 // CommandQueue loads commands into a queue
 type CommandQueue struct {
-	commands []queueItems
+	commands     []queueItems
+	currentIdx   int
+	currentDepth int
 }
 
 // NewCommandQueue returns a CommandQueue of a given size. Defaults to 1500
@@ -23,4 +25,26 @@ func NewCommandQueue(s ...int) *CommandQueue {
 		commands: c,
 	}
 	return &cq
+}
+
+func (cq *CommandQueue) calculateDepth(command common.Commands) {
+	if cq.currentDepth == 0 && command == common.LoopEnd {
+		// TODO: throw error
+	} else if command == common.LoopEnd {
+		cq.currentDepth--
+	} else if command == common.LoopStart {
+		cq.currentDepth++
+	}
+}
+
+// AddCommand adds a command to the queue
+func (cq *CommandQueue) AddCommand(command common.Commands) {
+	cq.calculateDepth(command)
+	qi := queueItems{command: command, depth: cq.currentDepth}
+	if cq.currentIdx < len(cq.commands) {
+		cq.commands[cq.currentIdx] = qi
+	} else {
+		cq.commands = append(cq.commands, qi)
+	}
+	cq.currentIdx++
 }
--- queue/queue__test.go
+++ queue/queue__test.go
@@ -1,6 +1,10 @@
 package queue
 
-import "testing"
+import (
+	"testing"
+
+	"../common"
+)
 
 func TestNewCommandQueue(t *testing.T) {
 	// Should create a CommandQueue and default to a size opf 1500
@@ -16,4 +20,44 @@ func TestNewCommandQueueSize(t *testing.T) {
 	if cq == nil || len(cq.commands) != customSize {
 		t.Error("CommandQueue should default to a size of 1500")
 	}
+}
+
+func TestAddCommand(t *testing.T) {
+	// Should add a commands Enum to the queue
+	cq := NewCommandQueue()
+	cq.AddCommand(common.Right)
+
+	if cq.commands[0].command != common.Right {
+		t.Error("Could not add comamnd to queue")
+	}
+}
+
+func TestAddCommandResizeSlice(t *testing.T) {
+	// Should automatically resize commands slice when inital size is execeeded
+	cq := NewCommandQueue(1)
+	cq.AddCommand(common.Right)
+	cq.AddCommand(common.Left)
+
+	if cq.commands[1].command != common.Left && len(cq.commands) != 2 {
+		t.Error("Commands slice did not automatically resize")
+	}
+}
+
+func TestCalcuateDepth(t *testing.T) {
+	cq := NewCommandQueue()
+	depthBefore := cq.currentDepth
+	cq.AddCommand(common.LoopStart)
+	depthAfter := cq.currentDepth
+
+	if depthBefore != 0 || depthAfter != 1 {
+		t.Errorf("Expected depth to be %d, instead got %d", 1, depthAfter)
+	}
+}
+
+// test that added commands have a proper depth assinged
+
+func TestCalcuateDepthError(t *testing.T) {
+	cq := NewCommandQueue()
+	cq.AddCommand(common.LoopEnd)
+	// Should panic/throw error
 }
